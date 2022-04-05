34a873abfe53a205f1acd241610397cf4b764eec
joao costa <joaovaraujoc@gmail.com>
2022-04-05 10:13:32 -0300

    solucao ruim

---
+++ src/application/Program.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import motel.entities.Reservation;
+
+public class Program {
+	public static void main(String args[]) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		List<Reservation> listaReservas = new ArrayList<>();
+
+		System.out.println("Room Number: ");
+		int number = sc.nextInt();
+		System.out.println("Check-in date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.println("Check-out date (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+
+		if (!checkOut.after(checkIn)) {
+			System.out.println("DATA ERRADA TA SAINDO ANTES DE ENTRAR");
+		} else {
+			Reservation reserva = new Reservation(number, checkIn, checkOut);
+			listaReservas.add(reserva);
+			System.out.println("Reserva confirmada com sucesso!");
+			System.out.println(reserva.toString());
+			System.out.println();
+			System.out.println("Atualizar reserva");
+			System.out.println("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.println("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+
+			Date now = new Date();
+			if (checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("ERRO! AS DATAS DEVEM SER FUTURAS");
+			} else if (!checkOut.after(checkIn)) {
+				System.out.println("DATA ERRADA TA SAINDO ANTES DE ENTRAR");
+			} else {
+				reserva.updateDates(checkIn, checkOut);
+				System.out.println(reserva.toString());
+			}
+		}
+	}
+}
